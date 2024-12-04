bdbb261b02a322604bf8a41a539dd942fff7ed47
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2024-12-04 20:30:12 +0000

    Initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-  "fmt"
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
 )
 
+func download(url *string) string {
+	cmd := exec.Command("yt-dlp", "-x", "--audio-format","wav", *url)
+  fmt.Println("Command args: ", cmd.Args)
+	if *url == "" {
+		fmt.Println("You must provide a url")
+		return ""
+	}
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatal("Error excecuting video download")
+		return ""
+	}
+	return string(output)
+}
+
 func main() {
-  fmt.Println("Downloading video...")
+	url := flag.String("u", "", "Youtube URL to download")
+	flag.Parse()
+	output := download(url)
+	fmt.Printf("Arguments: %s \n", flag.Args())
+	fmt.Println("Command output:", string(output))
 }
